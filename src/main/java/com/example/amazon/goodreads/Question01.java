f39eccf0047e6562a28e46797f16e75ed947328a
cpereira <cpereira@groupon.com>
2017-09-03 22:44:29 -0400

    questions from amazon online assessment

---
+++ src/main/java/com/example/amazon/goodreads/Question01.java
@@ -0,0 +1,34 @@
+package com.example.amazon.goodreads;
+
+/**
+ <p>
+ Bob’s fruit company is running a promotion in which customers receive prizes for purchasing a secret combination of items. The combination will change each day, so Bob wants to use a code list to make it easy to change the combination. The code list contains groups of fruits. Both the order of the groups within the code list and the order of the fruits within the groups matter. However, between the groups of fruits, any number, and type of fruit is allowable. The term “anything” is used to allow for any type of fruit to appear in that location within the group.
+ <br>
+ <br>
+ Consider the following secret code list: [[apple, apple], [banana, anything, banana]]<br>Based on the above secret code list, a customer who made either of the following purchases would win the prize:
+ <br>
+ orange, apple, apple, banana, orange, banana
+ <br>
+ apple, apple, orange, orange, banana, apple, banana, banana
+ <br>
+ <br>
+ Write an algorithm to output 1 if the customer is a winner else output 0.
+ </p>
+ */
+// IMPORT LIBRARY PACKAGES NEEDED BY YOUR PROGRAM
+// SOME CLASSES WITHIN A PACKAGE MAY BE RESTRICTED
+// DEFINE ANY CLASS AND METHOD NEEDED
+import java.util.List;
+// CLASS BEGINS, THIS CLASS IS REQUIRED
+
+class Solution01
+{
+    // METHOD SIGNATURE BEGINS, THIS METHOD IS REQUIRED
+    public int checkWinner(List<List<String>> codeList,
+                           List<String> shoppingCart)
+    {
+        // WRITE YOUR CODE HERE
+        return 0;
+    }
+    // METHOD SIGNATURE ENDS
+}
